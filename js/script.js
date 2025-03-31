408fc446a135b3d2cd9ddad6e1fd90bfc86a357b
mannuccimattia <mannuccimattia@gmail.com>
2025-03-31 15:44:37 +0200

    [UPD][FIX] Done memberCard js + Fixed card style

--- js/script.js
+++ js/script.js
@@ -36,3 +36,35 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+// funzione per creare la singola card del membro del team
+const createMemberCard = (member) => {
+  const {name, role, email, img} = member
+
+  const card = `<div class="col-12 col-md-6 col-lg-4">
+                  <div class="d-flex">
+                    <img src="./${img}" alt="" class="img-fluid">
+                    <div class="d-flex flex-column justify-content-center" id="card-text">
+                      <h4>${name}</h4>
+                      <p>${role}</p>
+                      <a href="">${email}</a>
+                    </div>
+                  </div>
+                </div>`;
+
+  return card;                
+};
+
+// funzione per renderizzare il contenuto di card
+const renderTeam = (array) => {
+  let cards = "";
+
+  for(let i=0; i<array.length; i++){
+    cards += createMemberCard(array[i]);
+  }
+
+  document.getElementById("team-members").innerHTML = cards
+}
+
+// chiamo renderTeam passandole l'attay teamMembers
+renderTeam(teamMembers);
